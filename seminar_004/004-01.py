ac52c8560d7e2edb2f331cd8fd62109e46d53472
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-10-15 20:33:08 +0300

    2

--- seminar_004/004-01.py
+++ seminar_004/004-01.py
@@ -1,3 +1,7 @@
-i = "1 5 6 7 5 2 9"
-result = list(map(int, (i.split()))) # list - преобразование в список // map - итерирует по строке // split - разделяем строку
+i = '1 5 6 7 5 2 9'
+result = list(map(int, (i.split()))) # list - преобразование в список // map - функция итерирует по строке // split - разделяем строку
 print(max(result), min(result))
+
+num = '2 54 6  7 8  89 56 3'
+my_list = [int(num) for num in num.split()]
+print(max(my_list), min(my_list))
