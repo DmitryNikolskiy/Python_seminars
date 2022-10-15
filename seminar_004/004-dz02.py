012aeb6213b0a5e948bb8d9efb8bcaba4242405f
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-10-15 23:01:32 +0300

    +

--- seminar_004/004-dz02.py
+++ seminar_004/004-dz02.py
@@ -5,7 +5,7 @@ my_list = []
 for i in range(2, int(n**0.5)):
     if n % i == 0:
         count = 1
-        for j in range(2, (i//2 + 1)):
+        for j in range(2, (i//2+1)):
             if i % j == 0:
                 count = 0
                 break
