a4fc4015d9bc3f4d4e871fb38e35a0de864dbdad
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-10-16 19:39:47 +0300

    seminar_005-02

---
+++ seminar_005/005-02.py
@@ -0,0 +1,15 @@
+# Дан список чисел. Создайте список, в который попадают числа, составляющие возрастающую последовательность. Порядок элементов менять нельзя
+#
+
+def nextmax(f_list):
+    max = f_list[0]
+    res = [f_list[0]]
+    for i in range(len(f_list)):
+        if f_list[i] > max:
+            max = f_list[i]
+            res.append(max)
+    return res
+
+my_list = [1, 5, 2, 3, 4, 6, 1, 7]
+print(my_list)
+print(nextmax(my_list))
