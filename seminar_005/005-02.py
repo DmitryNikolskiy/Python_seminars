7a1336b5947ac4c2b84dddf5d2a4da95ed1490d8
Dmitry Nikolskiy <nikolskiy@inbox.ru>
2022-10-16 19:49:34 +0300

    + edit

--- seminar_005/005-02.py
+++ seminar_005/005-02.py
@@ -8,8 +8,10 @@ def nextmax(f_list):
         if f_list[i] > max:
             max = f_list[i]
             res.append(max)
+    if len(res) == 1:                          # условие если сначала максимальное и оно одно
+        res = nextmax(f_list[1:])              # убираем и считаем дальше
     return res
 
-my_list = [1, 5, 2, 3, 4, 6, 1, 7]
+my_list = [11, 1, 5, 4, 3, 4, 6, 1, 7]
 print(my_list)
 print(nextmax(my_list))
